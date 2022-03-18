d1e3cae9474172a5031c01f820062789fcdecb6e
shinemost <supertain147@163.com>
2022-03-18 16:58:34 +0000

    use gin as web

--- db/sqlc/account.sql.go
+++ db/sqlc/account.sql.go
@@ -110,10 +110,12 @@ func (q *Queries) GetAccountForUpdate(ctx context.Context, id int64) (Account, e
 
 const listAccounts = `-- name: ListAccounts :many
 SELECT id, owner, balance, currency, created_at FROM accounts
-WHERE owner = $1
+-- WHERE owner = $1
 ORDER BY id
-LIMIT $2
-OFFSET $3
+LIMIT $1
+--LIMIT $2
+OFFSET $2
+--OFFSET $3
 `
 
 type ListAccountsParams struct {
@@ -123,7 +125,8 @@ type ListAccountsParams struct {
 }
 
 func (q *Queries) ListAccounts(ctx context.Context, arg ListAccountsParams) ([]Account, error) {
-	rows, err := q.db.QueryContext(ctx, listAccounts, arg.Owner, arg.Limit, arg.Offset)
+	// rows, err := q.db.QueryContext(ctx, listAccounts, arg.Owner, arg.Limit, arg.Offset)
+	rows, err := q.db.QueryContext(ctx, listAccounts,  arg.Limit, arg.Offset)
 	if err != nil {
 		return nil, err
 	}
