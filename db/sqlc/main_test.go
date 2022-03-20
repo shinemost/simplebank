12b1d1e79973e0d61de59fae68ae874e518a79dc
shinemost <supertain147@163.com>
2022-03-20 14:17:10 +0000

    handle db errors in go

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -13,13 +13,18 @@ import (
 var testQueries *Queries
 var testDB *sql.DB
 
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+)
+
 func TestMain(m *testing.M) {
 	config, err := util.LoadConfig("../..")
 	if err != nil {
 		log.Fatal("cannot load config:", err)
 	}
 
-	testDB, err := sql.Open(config.DBDriver, config.DBDriver)
+	testDB, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
