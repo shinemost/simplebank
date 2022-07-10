9b0e756f876ce91ea3ac936be57fcb1d2ee7c1f0
hjfu <supertain147@163.com>
2022-07-10 21:40:32 +0800

    add user session with refresh token

--- token/maker.go
+++ token/maker.go
@@ -3,7 +3,7 @@ package token
 import "time"
 
 type Maker interface {
-	CreateToken(username string, duartion time.Duration) (string, error)
+	CreateToken(username string, duartion time.Duration) (string, *Payload, error)
 
 	VerifyToken(token string) (*Payload, error)
 }
